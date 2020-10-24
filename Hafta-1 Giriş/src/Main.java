312a65b1874eb0c433b2ea775b8492302068dc6d
Ahmet Buğra Yiğiter <54077855+yigiterbugra@users.noreply.github.com>
2020-10-24 11:53:09 +0300

    İlk hafta

---
+++ Hafta-1 Giriş/src/Main.java
@@ -0,0 +1,66 @@
+public class Main {
+
+    public static void main(String[] args) {
+        /*System.out.print("Gazi");
+        System.out.print("Üniversitesi");
+        System.out.println("\n");
+        System.out.println("************");
+        System.out.println("Gazi");
+        System.out.println("Üniversitesi");*/
+        //Burada System.out.println ile System.out.print'in farkını gördük.
+        //Gazi Üniversitesi
+        /*int myNum = 10;
+
+        System.out.println("Gazi");
+        System.out.println(myNum);
+        double myDouble = 10.0;
+        System.out.println(myDouble);*/
+        //Narrowing casting
+        /*double myDouble = 9.78;
+        int myInt = (int)myDouble;
+        System.out.println(myDouble);
+        System.out.println(myInt);*/
+        //Widening casting
+        /*int myInt = 9;
+        double myDouble = myInt; //9.0
+        System.out.println(myDouble);*/
+        //String Metodlarımız.
+        //String myString = "Gazi Üniversitesi";
+        //String myString2 = "Gazi Ünİversitesi";
+        //System.out.println(myString.length());
+        //System.out.println(myString.toUpperCase());
+        //System.out.println(myString.toLowerCase());
+        //System.out.println(myString.lastIndexOf("i"));
+        /*String a = myString.toLowerCase();
+        String b = myString2.toLowerCase();
+        System.out.println(a.equals(b));*/
+        /*String a = "Gazi ";
+        String b = "Üniversitesi";*/
+
+        //String uniAdi = a + " " + b + " " +c ;
+        //System.out.println(uniAdi);
+        //System.out.println(a.concat(b));
+        //System.out.println(Math.max(3,4));
+        //System.out.println(Math.min(3,4));
+        //System.out.println(Math.sqrt(81));
+        //System.out.println(Math.abs(-15.3));
+        //int myNumber = (int)(Math.random()*101);
+        //System.out.println(myNumber);
+        /*int a = 36;
+        int b = 12;
+        double c = -19.3;
+        //System.out.println(Math.max(a,b));
+        //System.out.println(Math.min(a,b));
+        //System.out.println(Math.sqrt(a));
+        System.out.println(Math.abs(c));*/
+        int x = 11;
+        //int y = 7;
+        System.out.println(x == 9);
+
+
+    }
+}
+//print("Hello Online Word")
+//Console.WriteLine("Hello Online World");
+//printf("Hello Online World");
+//console.log("Hello Online World");
